d79b2680e3617231e2cef4fa482cf9bdc2a41c9f
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2022-05-20 15:35:00 +0200

    Fixed segfault in test destruction.

--- hector_ros2_utils/test/communication.cpp
+++ hector_ros2_utils/test/communication.cpp
@@ -89,6 +89,7 @@ int main( int argc, char **argv )
                            } );
   int result = RUN_ALL_TESTS();
   rclcpp::shutdown();
+  node.reset();
   node_thread.join();
   return result;
 }
